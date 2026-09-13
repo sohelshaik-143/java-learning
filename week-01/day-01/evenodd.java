b710d755f2071d32ecc8757da512093f784acd4d
shaik Sohel <shaikimambasha968@gmail.com>
2026-09-13 21:47:10 +0530

    Add evenodd.java to check even or odd numbers

---
+++ week-01/day-01/evenodd.java
@@ -0,0 +1,10 @@
+ class main{
+  public static void main(String[] args){
+    int i=23;
+    if(i%2==0){
+      System.out.println("true");
+    }else{
+     System.out.println("false");
+    }
+  }
+ }
