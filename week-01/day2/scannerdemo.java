753ae9482883b4f628967a47c52651f52e6a9ee8
shaik Sohel <shaikimambasha968@gmail.com>
2026-09-14 21:06:49 +0530

    Create scannerdemo.java

---
+++ week-01/day2/scannerdemo.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+scanner sc=new scanner();
+system.out.print("enter your age");
+int age =sc.nextint();
+system.out.println("age"+age);
+
+here it is te intution behind it
+  enter your age --> age =20-->scanner stores-->execute on program
+
+  important methods::1. nextInt(); // integer 
+                      2. nextDouble(); // decimal
+                      3. nextBoolean(); //true or false
+                      4.nextLine(); // new complete line 
